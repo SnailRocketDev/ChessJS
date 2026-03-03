104aed7e1958fb8eb28a4fb129c7a0951d3c8ba1
James Potts <jamiehp11@gmail.com>
2026-03-03 16:51:41 +0000

    Initial Commit

---
+++ startChess.js
@@ -0,0 +1,96 @@
+// NOTE: this example uses the chess.js library:
+// https://github.com/jhlywa/chess.js
+
+import { Chess } from '/chess.js'
+
+$(window).resize(myBoard.resize)
+var board = null
+var game = new Chess()
+var $status = $('#status')
+var $fen = $('#fen')
+var $pgn = $('#pgn')
+
+function onDragStart (source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  if (game.isGameOver()) return false
+
+  // only pick up pieces for the side to move
+  if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+    return false
+  }
+}
+
+function onDrop (source, target) {
+  try {
+      if (target === "offboard") {
+      return 'snapback'
+    } 
+    // see if the move is legal
+    var move = game.move({
+      from: source,
+      to: target,
+      promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    })
+  } catch(e) {
+    console.warn("BAD THINGS");
+    //console.log(e);
+    return 'snapback'
+  }
+
+  // illegal move
+
+  if (move === null) return 'snapback'
+  updateStatus()
+}
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd () {
+  board.position(game.fen())
+}
+
+function updateStatus () {
+  var status = ''
+
+  var moveColor = 'White'
+  if (game.turn() === 'b') {
+    moveColor = 'Black'
+  }
+
+  // checkmate?
+  if (game.isCheckmate()) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.'
+  }
+
+  // draw?
+  else if (game.isDraw()) {
+    status = 'Game over, drawn position'
+  }
+
+  // game still on
+  else {
+    status = moveColor + ' to move'
+
+    // check?
+    if (game.isCheck()) {
+      status += ', ' + moveColor + ' is in check'
+    }
+  }
+
+  $status.html(status)
+  $fen.html(game.fen())
+  $pgn.html('<span>' + game.pgn() + '</span>')
+}
+
+var config = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+}
+board = Chessboard('myBoard', config)
+
+
+updateStatus()
